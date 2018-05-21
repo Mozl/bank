4198f752f1f64a4853fcfa17678b472535699435
Louis Moselhi <moselhil@gmail.com>
2018-05-21 16:13:48 +0100

    added test for print deposit

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -5,4 +5,9 @@ describe Statement do
     statement = Statement.new
     expect(statement.print_balance).to eq "balance £0"
   end
+
+  it 'prints date' do
+    statement = Statement.new
+    expect(statement.print_deposit).to eq "credit £5"
+  end
 end
