8887412bb0cefd007fc24c8276ee9bbdc238c2c5
Louis Moselhi <moselhil@gmail.com>
2018-05-21 13:02:59 +0100

    added test and code for formatted date

--- lib/account.rb
+++ lib/account.rb
@@ -1,20 +1,24 @@
 class BankAccount
 
-  attr_reader :account
+  attr_reader :account, :date
 
   def initialize
     @account = []
+    @date = Time.new.strftime("%d-%m-%Y")
   end
 
   def deposit(amount)
     @account << amount
+    puts "£#{amount} has been added to your account."
   end
 
   def withdraw(amount)
     @account << amount
+    puts "£#{amount} has been withdrawn from your account."
   end
 
   def balance
     account.inject(0, :+)
   end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,16 +1,22 @@
 require './lib/account.rb'
 
 describe BankAccount do
-    it 'adds money to an account' do
-        account = BankAccount.new
-        account.deposit(10)
-        expect(account.balance).to eq(10)
-    end
+  it 'adds money to an account' do
+    account = BankAccount.new
+    account.deposit(10)
+    expect(account.balance).to eq(10)
+  end
 
-    it 'withdraws money from an account' do
-        account = BankAccount.new
-        account.deposit(100)
-        account.withdraw(-5)
-        expect(account.balance).to eq(95)
-    end
+  it 'withdraws money from an account' do
+    account = BankAccount.new
+    account.deposit(100)
+    account.withdraw(-5)
+    expect(account.balance).to eq(95)
+  end
+
+  it 'tests the date' do
+    time = Time.new
+    account = BankAccount.new
+    expect(account.date).to eq (time.strftime("%d-%m-%Y"))
+  end
 end
